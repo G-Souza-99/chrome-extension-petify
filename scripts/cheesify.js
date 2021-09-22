84868f9ec43ec18d83e474265b3cd0b0a980d942
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-22 19:08:29 +0100

    added files

---
+++ scripts/cheesify.js
@@ -0,0 +1,6 @@
+function cheesify() {
+  // TODO: Add the image replacement script here
+}
+
+// TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
+  
---
+++ scripts/popup.js
@@ -0,0 +1,5 @@
+function sendCheesifyMsg() {
+  // TODO: Write a function to send a message to the active tab to 'cheesify' it
+}
+
+// TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
