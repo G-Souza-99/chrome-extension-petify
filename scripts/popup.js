1b6483a8c51a81dd7d4129f0fda92e3d7e749ba9
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-22 19:34:36 +0100

    added extra

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,9 @@
 function sendCheesifyMsg() {
   // TODO: Write a function to send a message to the active tab to 'cheesify' it
+  alert("Cheesify")
 }
 
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
+document.querySelector('#cheesify').addEventListener('click', () => {
+  sendCheesifyMsg();
+})
