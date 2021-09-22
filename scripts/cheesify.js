c041a5c012c9501dadafd171468dd28508f2c78d
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-22 19:13:34 +0100

    cheesify start

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,12 @@
 function cheesify() {
   // TODO: Add the image replacement script here
+  document.querySelectorAll("img").forEach((img) => {
+    img.src = `https://source.unsplash.com/${img.width}x${
+      img.height
+    }/?cheese&${Math.random()}`;
+    img.srcset = img.src;
+  });
+
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
