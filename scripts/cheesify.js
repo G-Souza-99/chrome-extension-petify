a1b23e5b0dba5767df0fb1be024ea587b49ce1ec
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-22 19:38:28 +0100

    final commits

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,11 +1,9 @@
 const cheesify = () => {
   // TODO: Add the image replacement script here
-  document.querySelectorAll("img").forEach((img) => {
-    img.src = `https://source.unsplash.com/${img.width}x${
-      img.height
-    }/?cheese&${Math.random()}`;
+  document.querySelectorAll('img').forEach( (img) => {
+    img.src = `https://source.unsplash.com/${img.width}x${img.height}/?cheese&${Math.random()}`;
     img.srcset = img.src;
-  });
+  })
 
   document.querySelectorAll("img").forEach((img) => {
     img.src = `https://source.unsplash.com/${img.width}x${
@@ -15,5 +13,8 @@ const cheesify = () => {
   });
 }
 
-cheesify();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "cheesify") cheesify();
+});
+
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
