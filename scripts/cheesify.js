23e97ea1a12e3853c731aa4339ee94e2ecb3d6cd
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-22 19:53:18 +0100

    extension finished

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,5 +1,4 @@
 const cheesify = () => {
-  // TODO: Add the image replacement script here
   document.querySelectorAll('img').forEach( (img) => {
     img.src = `https://source.unsplash.com/${img.width}x${img.height}/?cheese&${Math.random()}`;
     img.srcset = img.src;
@@ -16,5 +15,3 @@ const cheesify = () => {
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "cheesify") cheesify();
 });
-
-// TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,9 @@
 const sendCheesifyMsg = () => {
-  // TODO: Write a function to send a message to the active tab to 'cheesify' it
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "cheesify" });
   });
 }
 
-// TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
 document.querySelector('#cheesify').addEventListener('click', () => {
   sendCheesifyMsg();
 })
